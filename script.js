4bea4a880da02bd15ee3ee6655df8dbc4a1330f5
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-16 13:10:47 -0300

    Regra de negócio

---
+++ script.js
@@ -0,0 +1,4 @@
+// carne - 400gr por pessoa   + de 6horas - 650gr
+// Cerveja - 1200ml por pessoa + 6horas - 2000ml
+// Refrigente/agua - 1000ml por pessoa + 6horas 1500ml
+// crianças valem por 0,5
