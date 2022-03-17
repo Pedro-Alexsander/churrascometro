169b89df651ab6e80384e434bb31c17774fd7796
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-17 16:18:06 -0300

    CSS do resultado

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
 // Cerveja - 1200ml por pessoa + 6horas - 2000ml
 // Refrigente/agua - 1000ml por pessoa + 6horas 1500ml
 // crianças valem por 0,5
+
+
+
+function calc(){
+    let adultos = parseInt(document.getElementById("adultos").value)
+    let criancas = (parseInt(document.getElementById("criancas").value)/2)
+    let tempo = parseFloat(document.getElementById("duracao").value)
+    let carne = (adultos * 0.4) + (criancas * 0.4)
+    let cerveja = adultos * 1.2
+    let refrigente = (adultos* 1)+(criancas * 1)
+    let mensagem = document.getElementById("resultado").innerHTML = " "
+
+    if(tempo <6){
+    mensagem = document.getElementById("resultado").innerHTML=
+        `<p>${carne}kg de carne</p><br> 
+        ${cerveja} litros de cerveja`
+        
+
+    }if(tempo>6){
+        carne = (adultos * 0.65) + (criancas * 0.65)
+        cerveja = adultos * 2
+        refrigente = (adultos* 1.5)+(criancas * 1.5)
+
+        mensagem = document.getElementById("resultado").innerHTML=
+        " "
+    }
+    }
